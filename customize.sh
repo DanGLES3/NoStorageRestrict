ce7aaaccc7709b89a15ca75d89a6a02d11c245e3
Τ Ι Λ ϟ Η <MrCarb0n@proton.me>
2022-06-02 13:36:46 +0600

    improve code quality and cleanup leftovers
    - case statement for faster execution
    - proper cleanup of leftovers
    - fix markdown format in README.md
    
    Signed-off-by: Τ Ι Λ ϟ Η <MrCarb0n@proton.me>

--- customize.sh
+++ customize.sh
@@ -1,19 +1,22 @@
+CODENAME="$(getprop ro.build.version.sdk)"
+MODDIRPATH="$MODPATH/system/priv-app/ExternalStorageProvider"
 
+case "$CODENAME" in
+	32)
+		mkdir -p "$MODDIRPATH"
+		cp -rf "$MODPATH"/S2/* "$MODDIRPATH"
+		;;
+	31)
+		mkdir -p "$MODDIRPATH"
+		cp -rf "$MODPATH"/S/* "$MODDIRPATH"
+		;;
+	30)
+		mkdir -p "$MODDIRPATH"
+		cp -rf "$MODPATH"/R/* "$MODDIRPATH"
+		;;
+esac
 
-CODENAME=$(getprop ro.build.version.sdk)
-if [[ "$CODENAME" == "32" ]]; then
-  mkdir -p "$MODPATH"/system/priv-app/ExternalStorageProvider
-  cp -rf "$MODPATH"/S2/* "$MODPATH"/system/priv-app/ExternalStorageProvider
-elif [[ "$CODENAME" == "31" ]]; then
-  mkdir -p "$MODPATH"/system/priv-app/ExternalStorageProvider
-  cp -rf "$MODPATH"/S/* "$MODPATH"/system/priv-app/ExternalStorageProvider
-elif [[ "$CODENAME" == "30" ]]; then
-  mkdir -p "$MODPATH"/system/priv-app/ExternalStorageProvider
-  cp -rf "$MODPATH"/R/* "$MODPATH"/system/priv-app/ExternalStorageProvider
-fi
- 
-rm -rf "$MODPATH"/R
-rm -rf "$MODPATH"/S
+find "$MODPATH"/* -maxdepth 0 ! \( -name 'system' -o -name 'module.prop' \) -exec rm -rf {} \;
 
 REPLACE="
 /system/priv-app/ExternalStorageProvider
